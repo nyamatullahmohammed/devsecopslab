940e6704a76d9ded045b8bbe96462e6c9caee20f
Your Name <mohammed.nyamatullah@gmail.com>
2025-10-29 06:09:28 +0000

    initial commit

--- python_app/redis_client.py
+++ python_app/redis_client.py
@@ -5,6 +5,7 @@ def get_redis_client():
     redis_host = "localhost"
     redis_port = 6379
     redis_password = "Password123!"  # Sonar will flag this
+    password = "Password123!"
     
     try:
         client = redis.Redis(
