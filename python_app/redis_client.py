456696b543aa64f4139d81360b7dc668879d0f8a
Your Name <mohammed.nyamatullah@gmail.com>
2025-10-29 07:02:43 +0000

    initial commit

--- python_app/redis_client.py
+++ python_app/redis_client.py
@@ -4,13 +4,11 @@ def get_redis_client():
     # ❌ Intentionally hardcoded secret
     redis_host = "localhost"
     redis_port = 6379
-   # redis_password = "Password123!"  # Sonar will flag this
-        
+           
     try:
         client = redis.Redis(
             host=redis_host,
             port=redis_port,
-            #password=redis_password,
             decode_responses=True
         )
         client.ping()  # Force connection
